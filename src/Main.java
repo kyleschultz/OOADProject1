c2d5b067caf06b8a3af1e04ef5a7964c978faca4
pbhetwal <pari.bhetwal@gmail.com>
2019-09-13 14:12:00 -0600

    create

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    	ZooKeeper Zoe = new ZooKeeper();
+
+    	Feline Frank = new Feline('Frank');
+    	Feline Fiona = new Feline('Fiona');
+    	Zoe.addAnimal(Frank);
+    	Zoe.addAnimal(Fiona);
+
+    	Cat Catthew = new Cat('Catthew');
+    	Cat Carl = new Cat('Carl');
+    	Zoe.addAnimal(Catthew);
+    	Zoe.addAnimal(Carl);
+
+    	Canine Chloe = new Canine('Chloe');
+    	Canine Cassidy = new Canine('Cassidy');
+    	Zoe.addAnimal(Chloe);
+    	Zoe.addAnimal(Cassidy);
+
+    	Wolf William = new Wolf('William');
+    	Wolf Winnie = new Wolf('Winnie');
+    	Zoe.addAnimal(William);
+    	Zoe.addAnimal(Winnie);
+
+    	Zoe.wake();
+    	Zoe.rollCall();
+    	Zoe.feed();
+    	Zoe.letRoam();
+
+    	Zoe.closeZoo();
+
     }
 }
