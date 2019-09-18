0f308100a913e7506665210c0527fa380479cb7a
Kyle Schultz <kysc9699@colorado.edu>
2019-09-18 13:51:59 -0600

    gitignore

--- src/Cat.java
+++ src/Cat.java
@@ -1,21 +1,54 @@
+import java.util.Random;
+
 public class Cat extends Feline{
 
-    public Cat(){
+    private Random rand;
 
+    public Cat(){
+        rand = new Random();
     }
 
     public Cat(String name){
         super(name);
+        rand = new Random();
     }
 
-    @Override
+    public String catDoSomething(){
+        String result = "";
+        int randInt = rand.nextInt(4);
+        switch(randInt){
+            case 0:
+                result = eat();
+                break;
+            case 1:
+                result = makeNoise();
+                break;
+            case 2:
+                if(getAsleep() == true){
+                    result = setAsleep(false);
+                    break;
+                }
+                else{
+                    result = setAsleep(true);
+                    break;
+                }
+
+            case 3:
+                result = roam();
+                break;
+            default:
+                result = "";
+                break;
+        }
+        return result;
+    }
+
     public String eat() {
         String name = getName();
         String result = name + " is eating fancy feast";
         return result;
     }
 
-    @Override
     public String makeNoise(){
         String noise = "Prrrr";
         return noise;
