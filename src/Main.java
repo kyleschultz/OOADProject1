042822ad04d0df8b4d0f1389e0658b7600e71cc2
Kyle Schultz <kysc9699@colorado.edu>
2019-09-18 16:49:26 -0600

    ploymorphism

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,24 @@ public class Main {
 
     	Zookeeper Zoe = new Zookeeper();
 
-    	Feline Frank = new Feline("Frank");
-    	Feline Fiona = new Feline("Fiona");
+    	Animal Frank = new Feline("Frank");
+    	Animal Fiona = new Feline("Fiona");
     	Zoe.addAnimal(Frank);
     	Zoe.addAnimal(Fiona);
 
-    	Cat Catthew = new Cat("Catthew");
-    	Cat Carl = new Cat("Carl");
+
+    	Animal Catthew = new Cat("Catthew");
+		Animal Carl = new Cat("Carl");
     	Zoe.addAnimal(Catthew);
     	Zoe.addAnimal(Carl);
 
-    	Canine Chloe = new Canine("Chloe");
-    	Canine Cassidy = new Canine("Cassidy");
+		Animal Chloe = new Canine("Chloe");
+		Animal Cassidy = new Canine("Cassidy");
     	Zoe.addAnimal(Chloe);
     	Zoe.addAnimal(Cassidy);
 
-    	Wolf William = new Wolf("William");
-    	Wolf Winnie = new Wolf("Winnie");
+		Animal William = new Wolf("William");
+		Animal Winnie = new Wolf("Winnie");
     	Zoe.addAnimal(William);
     	Zoe.addAnimal(Winnie);
 
